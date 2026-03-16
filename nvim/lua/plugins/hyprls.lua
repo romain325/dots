47d11205f4d4575e307e563f2518fc9db2af8602
kelkchoz <romainolivier42@gmail.com>
2026-03-16 22:24:25 +0100

    WELCOME TO DUMMY DUMDUM WORLDW
    Signed-off-by: kelkchoz <romainolivier42@gmail.com>

--- nvim/lua/plugins/hyprls.lua
+++ nvim/lua/plugins/hyprls.lua
@@ -1,27 +1,8 @@
--- vim.filetype.add({
---   pattern = {
---     ["hypr.+%.conf"] = "hyprlang",
---     [".*%.hl"] = "hyprlang",
---   },
--- })
-
 return {
   "AstroNvim/astrolsp",
   ---@param opts AstroLSPOpts
   opts = function(plugin, opts)
     opts.servers = opts.servers or {}
-    vim.list_extend(opts.servers, { "hyprlang" })
-
-    -- opts.config = require("astrocore").extend_tbl(opts.config or {}, {
-    --   hyprlang = {
-    --     cmd = { "hyprls" },
-    --     settings = {
-    --       hyprls = {
-    --         preferIgnoreFile = true,
-    --         ignore = { "hyprlock.conf", "hypridle.conf" },
-    --       },
-    --     },
-    --   }
-    -- })
+    vim.list_extend(opts.servers, { "hyprls" })
   end,
 }
